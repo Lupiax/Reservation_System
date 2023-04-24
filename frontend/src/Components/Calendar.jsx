a9bf835e2e4b9d062c628921009dd9975bfbe261
Lupia <129526603+Lupiax@users.noreply.github.com>
2023-04-24 10:59:27 +0300

    Room update - Pt. 3

--- frontend/src/Components/Calendar.jsx
+++ frontend/src/Components/Calendar.jsx
@@ -4,6 +4,7 @@ import '../hack.css';
 import { Component } from 'react';
 import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import { areIntervalsOverlapping } from 'date-fns';
 
 import { reservationContext } from '../Contexts/ReservationContext';
 
@@ -37,6 +38,48 @@ class Calendar extends Component {
     this.context.setReservations(converted_reservations);
   };
 
+  postReservation = async (start, end) => {
+
+    const date = new Date(start - end);
+
+    // some client sided checks...
+    if (date.getHours() > 2)
+      return this.props.showMessageModal('You cannot reserve more than two hours.');
+
+    const result = await fetch('http://127.0.0.1:5000/reservation/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      body: JSON.stringify({
+        start: start,
+        end: end,
+      }),
+    }).then(result => result.json());
+
+    if (!result.status) {
+      return this.props.showMessageModal(result.message);
+    }
+
+    if (result.update) {
+      // disgusting, needs refactoring.
+      let items = [...this.context.reservations];
+      let index = items.findIndex(
+        x =>
+          x.start.getDate() === new Date(result.reservation.start).getDate() &&
+          x.uuid === result.reservation.uuid
+      );
+      let item = { ...items[index] };
+      item.start = new Date(result.reservation.start);
+      item.end = new Date(result.reservation.end);
+      items[index] = item;
+      this.context.setReservations(items);
+    }
+    
+    this.props.showMessageModal(result.message);
+  };
+
   // I did not know this can be async too...
   async componentDidMount() {
     if (this.props.token === null) return this.props.showLoginModal();
@@ -62,52 +105,23 @@ class Calendar extends Component {
   };
 
   addReservation = async ({ start, end }) => {
-    const date = new Date(start - end);
 
-    // some client sided checks...
-    if (date.getHours() > 2)
-      return this.showMessageModal('You cannot reserve more than two hours.');
+    let overlap = false;
+    this.context.reservations.forEach((reservation) => {
 
-    const result = await fetch('http://127.0.0.1:5000/reservation/add', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-      body: JSON.stringify({
-        start: start,
-        end: end,
-      }),
-    }).then(result => result.json());
-
-    if (!result.status) return this.props.showMessageModal(result.message);
-
-    if (result.update) {
-      // disgusting, needs refactoring.
-      let items = [...this.context.reservations];
-      let index = items.findIndex(
-        x =>
-          x.start.getDate() === new Date(result.reservation.start).getDate() &&
-          x.uuid === result.reservation.uuid
+      const isOverlapping = areIntervalsOverlapping(
+        { start: new Date(reservation.start), end: new Date(reservation.end) },
+        { start: new Date(start), end: new Date(end) }
       );
-      let item = { ...items[index] };
-      item.start = new Date(result.reservation.start);
-      item.end = new Date(result.reservation.end);
-      items[index] = item;
-      this.context.setReservations(items);
-    } else {
-      this.context.setReservations([
-        ...this.context.reservations,
-        {
-          start: new Date(result.reservation.start),
-          end: new Date(result.reservation.end),
-          title: result.reservation.title,
-          uuid: result.reservation.uuid,
-        },
-      ]);
-    }
 
-    this.props.showMessageModal(result.message);
+      if (isOverlapping)
+        overlap = true;
+
+    })
+
+    if (overlap)
+      return this.postReservation(start, end); // don't show dialog if we're overlapping.
+    this.props.showReservationModal(start, end);
   };
 
   render() {
